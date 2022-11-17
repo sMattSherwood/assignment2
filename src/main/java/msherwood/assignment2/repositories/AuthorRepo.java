cf5687457efa1d5a45aa53dfc97e230c23cc5f8e
Matthew Sherwood <94009249+sherwoodMatthew@users.noreply.github.com>
2022-11-16 21:09:45 -0500

    added some of the abstract methods

--- src/main/java/msherwood/assignment2/repositories/AuthorRepo.java
+++ src/main/java/msherwood/assignment2/repositories/AuthorRepo.java
@@ -1,5 +1,7 @@
 package msherwood.assignment2.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,14 @@ import msherwood.assignment2.model.Authors;
 // this is where the author Repository is declared 
 @Repository
 public interface AuthorRepo extends CrudRepository<Authors,Integer> {
+
+    List<Authors> orderAuthorByLastName(String lastName);
+
+    List<Authors> orderAuthorByFirstName(String firstName);
+
+    List<Authors> searchAuthorByLastName(String lastname);
+
+    List<Authors> searchAuthorByFirstName(String firstName);
 
     
 }
